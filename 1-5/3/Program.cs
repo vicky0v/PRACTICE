043c7679091f96e31991fd662ff223c441f8ed17
vicky0v <107556583+vicky0v@users.noreply.github.com>
2022-06-16 12:08:12 +0300

    Add files via upload

---
+++ 1-5/3/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace з3
+{
+    class Program
+    {
+        class Converter
+        {
+            private double _rub, _usd, _eur;
+
+            public Converter(double usd, double eur, double rub)
+            {
+                _rub = rub;
+                _usd = eur;
+                _eur = eur;
+            }
+
+
+            public double ConvertTo(string valuta)
+            {
+                valuta = valuta.ToLower().Trim();
+                switch (valuta)
+                {
+                    case "rub":
+                        return _rub * 0.52;
+                    case "usd":
+                        return _usd * 29.63;
+                    case "eur":
+                        return _eur * 30.82;
+                    default:
+                        return 0;
+                }
+            }
+
+            public double ConvertFrom(double grivna, string valuta)
+            {
+                valuta = valuta.ToLower().Trim();
+                switch (valuta)
+                {
+                    case "rub":
+                        return grivna * 1.93;
+                    case "usd":
+                        return grivna * 0.034;
+                    case "eur":
+                        return grivna * 0.032;
+                    default:
+                        return 0;
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Сумма в рублях:");
+            double rub = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Сумма в долларах:");
+            double usd = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Сумма в евро:");
+            double eur = Convert.ToDouble(Console.ReadLine());
+
+            Converter con = new Converter(usd, eur, rub);
+            Console.WriteLine("Перевод из гривны - 1\nПеревод в гривны - 2");
+            string res = Console.ReadLine();
+            while (res == "1" || res == "2")
+            {
+                if (res == "1")
+                {
+                    Console.WriteLine("Количество гривен:");
+                    double g = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("Выберите валюту: введите rub, usd или eur");
+                    Console.WriteLine(con.ConvertFrom(g, Console.ReadLine()));
+                }
+                if (res == "2")
+                {
+                    Console.WriteLine("Выберите валюту: введите rub, usd или eur");
+                    Console.WriteLine(con.ConvertTo(Console.ReadLine()));
+                }
+                Console.WriteLine("Перевод из гривны - 1\nПеревод в гривны - 2");
+                res = Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ 1-5/4/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание4
+{
+    class Employee
+    {
+        public string name { get; }
+        public string surname { get; }
+        public double experience { get; set; }
+
+        private string post;
+        public static List<string> ListPost
+        {
+            get
+            {
+                return new List<string> { "Junior Developer", "Middle Developer", "Senior Developer" };
+            }
+        }
+
+        public Employee(string n, string s)
+        {
+            name = n;
+            surname = s;
+        }
+
+        public string Post
+        {
+            get
+            {
+                return post;
+            }
+            set
+            {
+                post = ListPost[Convert.ToInt32(value) - 1];
+            }
+        }
+
+        public double Salary
+        {
+            get
+            {
+                switch (post)
+                {
+                    case "Junior Developer":
+                        return experience * 30000;
+                    case "Middle Developer":
+                        return experience * 70000;
+                    case "Senior Developer":
+                        return experience * 100000;
+                    default:
+                        return 0;
+                }
+            }
+        }
+
+        public double Tax(double percent)
+        {
+            return Salary * percent / 100;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Задайте имя сотрудника:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Задайте фамилию сотрудника:");
+            string surname = Console.ReadLine();
+
+            Employee emp = new Employee(name, surname);
+
+            Console.WriteLine("Введите номер, соответствующую должности сотрудника:");
+            for (int i = 0; i < Employee.ListPost.Count; i++)
+                Console.WriteLine(i + 1 + " - " + Employee.ListPost[i]);
+            emp.Post = Console.ReadLine();
+
+            Console.WriteLine("Введите стаж сотрудника:");
+            emp.experience = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите процент налога:");
+            double per = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Фамилия и имя сотрудника: " + emp.surname + " " + emp.name + "\nДолжность: " + emp.Post + "\nОклад: " + emp.Salary + "\nНалоговый сбор: " + emp.Tax(per));
+            Console.Read();
+        }
+    }
+}
