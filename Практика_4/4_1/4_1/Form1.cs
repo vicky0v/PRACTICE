1614cd4e0b095f876612e82948802e02c56cb048
vicky0v <107556583+vicky0v@users.noreply.github.com>
2022-06-21 13:28:48 +0300

    Add files via upload

---
+++ Практика_4/4_1/4_1/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _4_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        void Info(int pay)
+        {
+            int credit = Convert.ToInt32(textBox1.Text);
+            int sum = credit - pay;
+            if (sum <= 0)
+            {
+                textBox1.Text = 0 + "";
+                MessageBox.Show("Долг отсутствует. Переплата: " + (pay - credit));
+            }
+            else
+                textBox1.Text = sum + "";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int pay = Convert.ToInt32(textBox2.Text);
+            Info(pay);
+        }
+    }
+}
