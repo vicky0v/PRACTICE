5b7fc14c767a1866d6e893b065b93b7221e9bbf1
vicky0v <107556583+vicky0v@users.noreply.github.com>
2022-06-21 13:38:51 +0300

    Add files via upload

---
+++ Практика_4/4_2/4_2/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _4_2
+{
+    public partial class Form1 : Form
+    {
+        static int Factorial(int a)
+        {
+            int z = 1;
+            do
+            {
+                if (a == 0)
+                {
+                    z = 1;
+                }
+                else
+                {
+                    z = a * z;
+                    a = a - 1;
+                }
+
+            } while (a > 0);
+            return z;
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int n = Convert.ToInt32(textBox1.Text);
+            res.Text = "Количество возможных вариантов доставки товара: " + Factorial(n);
+        }
+    }
+}
