9aec58b5da10f935be12d75dd06b0f47a2805a3a
vicky0v <107556583+vicky0v@users.noreply.github.com>
2022-06-22 12:05:49 +0300

    Add files via upload

---
+++ Практика_5/5_1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace _5_1
+{
+    class Program
+    {
+        static string path = @"C:\\Folder";
+        static void Main(string[] args)
+        {
+            DirectoryInfo directoryinfo = new DirectoryInfo("Folder");
+
+            for (int i = 0; i <= 100; i++)
+                if (!directoryinfo.Exists)
+                    Directory.CreateDirectory(path + i);
+
+            Console.WriteLine("Для удаления нажмите enter ");
+            Console.ReadLine();
+
+            for (int i = 0; i <= 100; i++)
+                if (!directoryinfo.Exists)
+                    Directory.Delete(path + i);
+            Console.WriteLine("~Все папки удалены~");
+            
+        }
+    }
+}
