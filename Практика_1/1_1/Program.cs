e763946193e3941fd4f8e2be2b4c2c8b447fcf07
vicky0v <107556583+vicky0v@users.noreply.github.com>
2022-07-04 09:26:08 +0300

    Add files via upload

---
+++ Практика_1/1_1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Rectangle
+    {
+        private double side1, side2;
+
+        public double Area
+        {
+            get { return this.AreaCalculator(); }
+        }
+
+        public double Perimeter
+        {
+            get { return this.PerimeterCalculator(); }
+        }
+        public Rectangle(double side1, double side2)
+        {
+            this.side1 = side1;
+            this.side2 = side2;
+        }
+
+        private double AreaCalculator()
+        {
+            return side1 * side2;
+        }
+
+        private double PerimeterCalculator()
+        {
+            return 2 * (side1 + side2);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double side1, side2;
+
+            Console.Write("Длина первой стороны: ");
+            side1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine();
+            Console.Write("Длина второй стороны: ");
+            side2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine();
+
+            Rectangle n1 = new Rectangle(side1, side2);
+            Console.WriteLine("Площадь прямоугольника равна {0}", n1.Area);
+            Console.WriteLine("Периметр прямоугольника равен {0}", n1.Perimeter);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Практика_1/1_2/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Point
+    {
+        string name;
+        int x, y;
+        public int X
+        {
+            get
+            {
+                return x;
+            }
+        }
+        public int Y
+        {
+            get
+            {
+                return y;
+            }
+        }
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+        }
+        public Point()
+        : this("", 0, 0) { }
+        public Point(string name, int x, int y)
+        {
+            Console.WriteLine("Введите название координаты:");
+            this.name = Console.ReadLine();
+            Console.WriteLine("Введите X - :");
+            this.x = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите Y - :");
+            this.y = int.Parse(Console.ReadLine());
+        }
+    }
+    class Figure
+    {
+        int number;
+        Point[] points;
+        string name;
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+        public void InitializeFigure()
+        {
+            Console.WriteLine("Введите количество вершин фигуры: ");
+            number = int.Parse(Console.ReadLine());
+            points = new Point[number];
+            for (int i = 0; i < points.Length; i++)
+            {
+                points[i] = new Point();
+            }
+
+        }
+        public double LengthSide(Point a, Point b)
+        {
+            return Math.Sqrt(Math.Pow(a.X - b.X, 2) + Math.Pow(a.Y - b.Y, 2));
+        }
+        public double CalculatePerimeter()
+        {
+            double perimeter = 0;
+            for (int i = 1; i < points.Length; i++)
+            {
+                perimeter += this.LengthSide(points[i - 1], points[i]);
+            }
+            perimeter += this.LengthSide(points[0], points[points.Length - 1]);
+            return perimeter;
+        }
+        public Figure(string name)
+        {
+            InitializeFigure();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Figure cube = new Figure("cube");
+            Console.WriteLine(cube.CalculatePerimeter());
+            Console.ReadKey();
+        }
+    }
+}
